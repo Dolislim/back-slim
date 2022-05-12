4ab8878076087cfe50645f208612207a547b147c
Thomas Savournin <tosave.vbl@gmail.com>
2022-05-12 13:16:10 +0200

    refactor: routes.php todos

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 use App\Application\Actions\User\ListUsersAction;
@@ -19,8 +20,58 @@ return function (App $app) {
         return $response;
     });
 
+    // Users / thirdparties
+    // Todo: Création utilisateur + client
+    // Todo: Vérification si existe déjà (email)
+    // Todo: Connexion avec récupération token
+    // Todo: Affichage d'un client (id)
+    // Todo: Modification informations personnelles (id)
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
+    });
+
+    // Proposals
+    // Todo: Création d'un devis
+    // Todo: Liste des devis
+    // Todo: Affichage d'un devis
+    $app->group('/proposals', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+    });
+
+    // Orders
+    // Todo: Liste des commandes
+    // Todo: Affichage d'une commande
+    // Todo: Faire un paiement
+    $app->group('/orders', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+    });
+
+    // Invoices
+    // Todo: Liste des factures
+    // Todo: Affichage d'une facture
+    $app->group('/invoices', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+    });
+
+    // Shipments
+    // Todo: Liste des expéditions
+    // Todo: Informations sur une expédition
+    $app->group('/shipments', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+    });
+
+    // Documents
+    // Todo: Liste des document d'une partie
+    // Todo: Affichage d'un document (download)
+    $app->group('/documents', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+    });
+
+    // Products
+    // Todo: Liste des produits
+    // Todo: Affichage d'un produit
+    $app->group('/products', function (Group $group) {
+        $group->get('', ListUsersAction::class);
     });
 };
