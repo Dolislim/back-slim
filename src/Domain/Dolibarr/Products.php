e8ec41649829cccc57adb8f16b62edd2f6003a20
Thomas Savournin <tosave.vbl@gmail.com>
2022-05-16 16:44:40 +0200

    feat: new Domain Products.php for product management

--- src/Domain/Dolibarr/Products.php
+++ src/Domain/Dolibarr/Products.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Domain\Dolibarr;
 
 use App\Domain\Dolibarr\Dolibarr;
-use App\Domain\Dolibarr\Documents;
 
 /**
  * Product management with Dolibarr API
@@ -15,40 +14,71 @@ final class Products extends Dolibarr
 {
     /**
      * GET a list of products
-     * "products?sortfield=t.ref&sortorder=ASC&limit=100&page=0&mode=1&category=0"
-     * @param int $limit Limit for list
-     * @param int $page Page number
+     * GET une liste de produits
+     * @param array $params ['sortfield', 'sortorder', 'limit', 'page', 'mode', 'category', 'sqlfilters', 'ids_only', 'variant_filter', 'pagination_data']
+     * @example "products?sortfield=t.ref&sortorder=ASC&limit=100&page=1"
+     * @uses string sortfield => 't.ref', Sort field
+     * @uses string sortorder => 'ASC', Sort order
+     * @uses int limit => 100, Limit for list
+     * @uses int page Page => 0, number
+     * @uses int mode, Use this param to filter list (0 for all, 1 for only product, 2 for only service)
+     * @uses int category, Use this param to filter list by category
+     * @uses string sqlfilters, Other criteria to filter answers separated by a comma. Syntax example "(t.tobuy=0) and (t.tosell=1)"
+     * @uses bool ids_only, Return only IDs of product instead of all properties (faster, above all if list is long)
+     * @uses int variant_filter, Use this param to filter list (0 = all, 1=products without variants, 2=parent of variants, 3=variants only)
+     * @uses bool pagination_data, If this parameter is set to true the response will include pagination data. Default value is false. Page starts from 0
      */
-    public function getAllProducts($category = 1, $limit = 100, $page = 0)
+    public function getAllProducts($params = [])
     {
-        $sortfield = 't.ref'; // Sort field
-        $sortorder = 'ASC'; // Sort order
-        $mode = 1; // 0 for all, 1 for only product, 2 for only service
+        $uri = 'products';
 
-        $data = $this->getDolibarr('products?sortfield=' . $sortfield . '&sortorder=' . $sortorder . '&limit=' . $limit . '&page=' . $page . '&category=' . $category); // On récupère la liste des produits
+        $default = [
+            'sortfield' => 't.ref',
+            'sortorder' => 'ASC',
+            'limit' => 100,
+            'page' => 0
+        ];
 
-        // Todo: Utiliser les collections de laravel
-        if (!isset($data['error'])) {
-            foreach ($data as $key => $value) // Pour chaque produit on ajoute une image
-            {
-                $documments = new Documents;
-                $image = $documments->getDocumentsById($value['id']);
-                if (isset($image['images'])) // Si une image est trouvée
-                {
-                    $data[$key]['image'] = $image['images'][0];
-                }
-            }
-        }
-
-        return $this->collectDolibarr($data);
+        $request = $this->createRequestDolibarr($uri, $params, $default);
+        return $this->getDolibarr($request);
     }
 
-    public function getProductById()
+    /**
+     * Return an array with product information
+     * Renvoie un tableau avec des informations sur le produit
+     * @param int $id ID of product
+     * @param array $params ['includestockdata', 'includesubproducts', 'includeparentid', 'includetrans']
+     * @example "products/1"
+     * @uses int includestockdata, Load also information about stock (slower)
+     * @uses bool includesubproducts, Load information about subproducts
+     * @uses bool includeparentid, Load also ID of parent product (if product is a variant of a parent product)
+     * @uses bool includetrans, Load also the translations of product label and description
+     */
+    public function getProductById($id, $params = [])
     {
+        $uri = 'products/' . $id;
+
+        $request = $this->createRequestDolibarr($uri, $params);
+        return $this->getDolibarr($request);
     }
 
-    public function getProductByRef()
+    /**
+     * Return an array with product information
+     * Renvoie un tableau avec des informations sur le produit
+     * @param int $ref Ref of element
+     * @param array $params ['includestockdata', 'includesubproducts', 'includeparentid', 'includetrans']
+     * @example "products/ref/UNEREF"
+     * @uses int includestockdata, Load also information about stock (slower)
+     * @uses bool includesubproducts, Load information about subproducts
+     * @uses bool includeparentid, Load also ID of parent product (if product is a variant of a parent product)
+     * @uses bool includetrans, Load also the translations of product label and description
+     */
+    public function getProductByRef($ref, $params = [])
     {
+        $uri = 'products/ref/' . $ref;
+
+        $request = $this->createRequestDolibarr($uri, $params);
+        return $this->getDolibarr($request);
     }
 
     public function getProductCategoriesById()
@@ -57,13 +87,5 @@ final class Products extends Dolibarr
 
     public function verifProductByIdRef()
     {
-    }
-
-    public function test($name = null)
-    {
-        return [
-            'Products' => 'Retour du Domain OK',
-            'Name' => $name
-        ];
     }
 }
